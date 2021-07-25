e47934176a9d560272b4a9dedcdae581247f266f
phandeyr <phandeyr@gmail.com>
2021-07-25 15:37:15 +0100

    added get, get all and update api handlers

--- todo-service/api/todo.js
+++ todo-service/api/todo.js
@@ -28,3 +28,52 @@ module.exports.create = async (event, context, callback) => {
     body: JSON.stringify({ 'message': 'Todo created successfully' })
   };
 };
+
+module.exports.get = async (event, context, callback) => {
+  const params = {
+    TableName: process.env.TODO_TABLE,
+    Key: {
+      ID: event.pathParameters.id
+    }
+  };
+
+  const result = await dynamodb.get(params).promise();
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(result)
+  };
+};
+
+module.exports.getAll = async (event, context, callback) => {
+  const params = {
+    TableName: process.env.TODO_TABLE
+  };
+
+  const result = await dynamodb.scan(params).promise();
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(result)
+  }
+};
+
+module.exports.update = async (event, context, callback) => {
+  const requestBody = JSON.parse(event.body);
+  const title = requestBody.title;
+
+  const params = {
+    TableName: process.env.TODO_TABLE,
+    Item: {
+      ID: event.pathParameters.id,
+      Title: title
+    }
+  };
+
+  await dynamodb.put(params).promise();
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ 'message': 'Todo updated successfully' })
+  };
+}
